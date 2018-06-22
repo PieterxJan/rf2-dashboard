008c18d5f1c0158e02a89f4b9d16c8a60d0afc2b
Pieter-Jan Claeysens <pieterjan@newance.be>
2018-06-22 21:01:03 +0200

    Added Laravel Passport

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::group(['middleware' => 'auth:api', 'prefix' => 'v1'], function() {
+    Route::get('fake', function() {
+        return response()->json(['hello']);
+    });
+
+    Route::get('info', function() {
+        return response()->json(['echte data']);
+    });
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
